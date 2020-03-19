641fe16fe923bb4633e319866c91d75177080a39
Simon Newton <simonanewton@gmail.com>
2020-03-19 07:17:06 -0400

    created a functional generatePassword function that works based on given parameters

--- script.js
+++ script.js
@@ -1,44 +1,58 @@
-
 // validates user inputs so that at least one variable is selected
-function validateInputs() {
+function validInputs(length, lowercase, uppercase, numbers, symbols) {
+	return ((length >= 8 && length <= 128) && (lowercase || uppercase || numbers || symbols));
 }
 
 // returns a random lowercase letter from Char Code
-function randomLowerCase() {
-    return String.fromCharCode(Math.floor((Math.random() * (122 - 97)) + 97));
+function randomLowercase() {
+	return String.fromCharCode(Math.floor((Math.random() * (122 - 97 + 1)) + 97));
 }
 
 // returns a random uppercase letter from Char Code
-function randomUpperCase() {
-    return String.fromCharCode(Math.floor((Math.random() * (90 - 65)) + 65));
+function randomUppercase() {
+	return String.fromCharCode(Math.floor((Math.random() * (90 - 65 + 1)) + 65));
 }
 
 // returns a random number from Char Code
 function randomNumber() {
-    return String.fromCharCode(Math.floor((Math.random() * (57 - 48)) + 48));
+	return String.fromCharCode(Math.floor((Math.random() * (57 - 48 + 1)) + 48));
 }
 
 // returns a random symbol from Char Code
 function randomSymbol() {
-    return String.fromCharCode(Math.floor((Math.random() * (47 - 32)) + 32));
+	return String.fromCharCode(Math.floor((Math.random() * (47 - 32 + 1)) + 32));
+}
+
+// returns an array of functions chosen based on user inputs
+function enabledFunctions(lowercase, uppercase, numbers, symbols) {
+	var enabledFunctionsArray = [];
+
+	if (lowercase) enabledFunctionsArray.push(randomLowercase);
+	if (uppercase) enabledFunctionsArray.push(randomUppercase);
+	if (numbers) enabledFunctionsArray.push(randomNumber);
+	if (symbols) enabledFunctionsArray.push(randomSymbol);
+	
+	return enabledFunctionsArray;
 }
 
 // initializes an empty string, then adds characters to it using a randomly selected function
-function assemblePassword(passwordMaxLength) {
-    var passwordString = "";
-	var passwordMinLength = 8;
-	var randomFunction = [randomLowerCase, randomUpperCase, randomNumber, randomSymbol];
-    for (var i=0; i < (Math.floor(Math.random() * (passwordMaxLength - passwordMinLength) + passwordMinLength)); i++) {
-        passwordString += randomFunction[Math.floor(Math.random() * randomFunction.length)];
-    }
-    return passwordString;
+function generatePassword(length, lowercase, uppercase, numbers, symbols) {
+	var passwordString = "";
+
+	var includedCharacters = enabledFunctions(lowercase, uppercase, numbers, symbols);
+
+	for (var i=0; i < length; i++) {
+		passwordString += includedCharacters[Math.floor(Math.random() * includedCharacters.length)]();
+	}
+
+	return passwordString;
 }
 
-// validates inputs from the user, then returns the randomly assembled password
-function generatePassword(length) {
-	validateInputs;
-    return assemblePassword(length);
-}
+
+// TO DO:
+// - modify HTML/CSS files to accept user character inputs
+// - convert user inputs to (length, lowercase, uppercase, numbers, symbols) values
+
 
 
 //-------------------------------------------------------
@@ -47,7 +61,15 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-	var password = generatePassword();
+	var length = 12;
+	var lowercase = true;
+	var uppercase = true;
+	var numbers = true;
+	var symbols = true;
+
+	console.log(validInputs(length, lowercase, uppercase, numbers, symbols));
+
+	var password = generatePassword(length, lowercase, uppercase, numbers, symbols);
 	var passwordText = document.querySelector("#password");
 	passwordText.value = password;
 }
